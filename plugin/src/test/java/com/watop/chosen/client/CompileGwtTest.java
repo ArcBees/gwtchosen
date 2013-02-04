9c2871c54e614a68981194c374c23f8e9b4622e9
Brandon Donnelson <branflake2267@gmail.com>
2013-02-04 14:53:54 -0800

    sample mods

---
+++ plugin/src/test/java/com/watop/chosen/client/CompileGwtTest.java
@@ -0,0 +1,14 @@
+package com.watop.chosen.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class CompileGwtTest extends GWTTestCase {
+    @Override
+    public String getModuleName() {
+        return "com.watopi.chosen.Chosen";
+    }
+
+    public void testSandbox() {
+        assertTrue(true);
+    }
+}
