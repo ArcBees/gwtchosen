473fa77a15646853b8e47592d9c0aa993e4573b5
Brandon Donnelson <branflake2267@gmail.com>
2013-02-04 17:23:01 -0800

    test fix for widgetsample

---
+++ sample/src/test/java/com/watopi/chosen/widgetsample/client/CompileWidgetSampleGwtTest.java
@@ -0,0 +1,14 @@
+package com.watopi.chosen.widgetsample.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class CompileWidgetSampleGwtTest extends GWTTestCase {
+    @Override
+    public String getModuleName() {
+        return "com.watopi.chosen.widgetsample.WidgetSample";
+    }
+
+    public void testSandbox() {
+        assertTrue(true);
+    }
+}
