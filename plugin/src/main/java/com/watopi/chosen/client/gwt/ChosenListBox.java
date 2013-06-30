c072fced1c9490ebc1565750a4533df93c618473
Julien Dramaix <julien.dramaix@gmail.com>
2013-06-30 10:19:03 +0200

    git fix issue #42

--- plugin/src/main/java/com/watopi/chosen/client/gwt/ChosenListBox.java
+++ plugin/src/main/java/com/watopi/chosen/client/gwt/ChosenListBox.java
@@ -1,15 +1,15 @@
 /*
  * Copyright (C) 2012 Julien Dramaix
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
  * including without limitation the rights to use, copy, modify, merge, publish, distribute,
  * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all copies or
  * substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
  * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
  * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
@@ -28,7 +28,6 @@ import com.google.gwt.event.dom.client.DomEvent.Type;
 import com.google.gwt.event.shared.EventHandler;
 import com.google.gwt.event.shared.LegacyHandlerWrapper;
 import com.google.gwt.i18n.client.HasDirection.Direction;
-import com.google.gwt.query.client.Function;
 import com.google.gwt.query.client.GQuery;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -253,6 +252,13 @@ public class  ChosenListBox extends ListBox implements HasAllChosenHandlers {
         if (update){
             update();
         }
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
+
+        update();
     }
 
     public void forceRedraw() {
