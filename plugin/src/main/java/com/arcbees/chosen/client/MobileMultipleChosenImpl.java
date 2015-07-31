6f8fe1c8d2629290726127aba929d76e854ab046
jasonlemay <jason.lemay@arcbees.com>
2015-07-31 11:43:58 -0400

    Add animation on deselect

--- plugin/src/main/java/com/arcbees/chosen/client/MobileMultipleChosenImpl.java
+++ plugin/src/main/java/com/arcbees/chosen/client/MobileMultipleChosenImpl.java
@@ -20,8 +20,11 @@ import com.arcbees.chosen.client.SelectParser.OptionItem;
 import com.arcbees.chosen.client.event.ChosenChangeEvent;
 import com.arcbees.chosen.client.event.MaxSelectedEvent;
 import com.google.gwt.dom.client.OptionElement;
+import com.google.gwt.query.client.Function;
 import com.google.gwt.query.client.GQuery;
 import com.google.gwt.user.client.Event;
+
+import static com.google.gwt.query.client.GQuery.$;
 
 public class MobileMultipleChosenImpl extends AbstractMobileChosenImpl {
     @Override
@@ -85,7 +88,21 @@ public class MobileMultipleChosenImpl extends AbstractMobileChosenImpl {
             option.setSelected(false);
         }
 
-        element.removeClass(getCss().resultSelected());
+        if (getOptions().isMobileAnimation()) {
+            final String paddingTop = element.css("padding-top");
+            final String paddingBottom = element.css("padding-bottom");
+            final int speed = getOptions().getMobileAnimationSpeed();
+
+            element.animate("height: 0, padding-top: 0, padding-bottom: 0", speed, new Function() {
+                public void f() {
+                    $(this).animate("height: auto, padding-top: " + paddingTop + ", padding-bottom: " + paddingBottom,
+                            speed);
+                    $(this).removeClass(getCss().resultSelected());
+                }
+            });
+        } else {
+            element.removeClass(getCss().resultSelected());
+        }
     }
 
     @Override
