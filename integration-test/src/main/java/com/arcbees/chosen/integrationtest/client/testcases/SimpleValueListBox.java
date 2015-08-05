ad2632dbbbaac8e5f85f2bbee0615ed49c5863a7
foal <stas@jresearch.org>
2015-08-05 18:40:06 +0200

    Update SimpleValueListBox.java

--- integration-test/src/main/java/com/arcbees/chosen/integrationtest/client/testcases/SimpleValueListBox.java
+++ integration-test/src/main/java/com/arcbees/chosen/integrationtest/client/testcases/SimpleValueListBox.java
@@ -27,6 +27,7 @@ import com.arcbees.chosen.integrationtest.client.domain.CarBrand;
 import com.google.common.collect.Lists;
 import com.google.gwt.text.shared.Renderer;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.view.client.ProvidesKey;
 
 import static com.arcbees.chosen.integrationtest.client.domain.DefaultCarRenderer.RENDERER;
 
@@ -51,7 +52,12 @@ public class SimpleValueListBox extends TestCase {
 
     @Override
     public void run() {
-        listBox = new ChosenValueListBox<CarBrand>(getRenderer(), options);
+        listBox = new ChosenValueListBox<CarBrand>(getRenderer(), new ProvidesKey<CarBrand>() {
+            @Override
+            public Object getKey(final CarBrand item) {
+                return item.name();
+            }
+        }, options);
 
         List<CarBrand> acceptableValues = Lists.newArrayList(CarBrand.values());
         if (addNullValue) {
