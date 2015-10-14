2993aa9f261304faf51188a50f3eeaa8ded789f3
kot <stas@jresearch.org>
2015-10-14 17:50:56 +0200

    Test Case to check

---
+++ integration-test/src/main/java/com/arcbees/chosen/integrationtest/client/testcases/IsAcceptedValueListBox.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright 2014 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.arcbees.chosen.integrationtest.client.testcases;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import com.arcbees.chosen.client.ChosenOptions;
+import com.arcbees.chosen.client.gwt.ChosenValueListBox;
+import com.arcbees.chosen.integrationtest.client.TestCase;
+import com.arcbees.chosen.integrationtest.client.domain.CarBrand;
+import com.google.common.collect.Lists;
+import com.google.gwt.text.shared.Renderer;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.view.client.ProvidesKey;
+
+import static com.arcbees.chosen.integrationtest.client.domain.DefaultCarRenderer.RENDERER;
+
+public class IsAcceptedValueListBox extends TestCase {
+    private final ChosenOptions options;
+    private final boolean addNullValue;
+
+    private ChosenValueListBox<CarBrand> listBox;
+
+    public IsAcceptedValueListBox() {
+        this(new ChosenOptions(), false);
+    }
+
+    protected IsAcceptedValueListBox(final boolean addNullValue) {
+        this(new ChosenOptions(), addNullValue);
+    }
+
+    protected IsAcceptedValueListBox(final ChosenOptions options, final boolean addNullValue) {
+        this.options = options;
+        this.addNullValue = addNullValue;
+    }
+
+    @Override
+    public void run() {
+        listBox = new ChosenValueListBox<CarBrand>(getRenderer(), new ProvidesKey<CarBrand>() {
+            @Override
+            public Object getKey(final CarBrand item) {
+                return item == null ? null : item.name();
+            }
+        }, options);
+
+        final List<CarBrand> acceptableValues = Lists.newArrayList(CarBrand.values());
+        if (addNullValue) {
+            acceptableValues.add(0, null);
+        }
+
+        listBox.setAcceptableValues(acceptableValues);
+
+        if (!listBox.isAccepted(CarBrand.AUDI)) {
+            throw new AssertionError("Audi is acceptable value");
+        }
+
+        RootPanel.get().add(listBox);
+    }
+
+    @Nullable
+    protected ChosenValueListBox<CarBrand> getListBox() {
+        return listBox;
+    }
+
+    protected Renderer<CarBrand> getRenderer() {
+        return RENDERER;
+    }
+}
